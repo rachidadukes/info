1c1af743824227cf0f7152f26167be3b49a81650
Rachida Dukes <rachidadukes@live.com>
2026-05-18 19:12:56 -0400

    Delete IHistoryService.cs

--- IHistoryService.cs
+++
@@ -1,9 +0,0 @@
-using MyApp.Domain.Entities;
-
-namespace MyApp.ReferenceData;
-
-public interface IHistoryService
-{
-    Task SaveAsync(HistoryModel entry, CancellationToken ct = default);
-    Task<IReadOnlyList<HistoryModel>> GetAllAsync(int take = 100, CancellationToken ct = default);
-}
