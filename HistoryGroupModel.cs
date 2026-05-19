2c58191c3a9c90a371367660367de48c9d96e9a6
Rachida Dukes <rachidadukes@live.com>
2026-05-19 18:51:11 -0400

    Delete HistoryGroupModel.cs

--- HistoryGroupModel.cs
+++
@@ -1,9 +0,0 @@
-namespace MyApp.Domain.Entities;
-
-public class HistoryGroupModel
-{
-    public string?   Description { get; set; }
-    public string?   Environment { get; set; }
-    public int       Count       { get; set; }
-    public DateTime  LastRun     { get; set; }
-}
